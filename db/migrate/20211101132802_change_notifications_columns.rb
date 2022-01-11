03f6bcff76fda91ee7c1681e0e0dfe85952dfb50
Niyomong <tt910208@gmail.com>
2022-01-11 19:48:00 +0900

    mib

--- db/migrate/20211101132802_change_notifications_columns.rb
+++ db/migrate/20211101132802_change_notifications_columns.rb
@@ -1,9 +1,9 @@
 class ChangeNotificationsColumns < ActiveRecord::Migration[6.0]
   def change
-    remove_column :notifications, :visiter_id, :references
-    remove_column :notifications, :visited_id, :references
-    remove_column :notifications, :post_id, :references
-    remove_column :notifications, :comment_id, :references
+    # remove_column :notifications, :visiter_id, :references
+    # remove_column :notifications, :visited_id, :references
+    # remove_column :notifications, :post_id, :references
+    # remove_column :notifications, :comment_id, :references
 
     add_column :notifications, :visiter_id, :integer
     add_column :notifications, :visited_id, :integer
