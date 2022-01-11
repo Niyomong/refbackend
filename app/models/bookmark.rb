c044784a857a872b7ce08e8ed4775f271be88b67
Niyomong <tt910208@gmail.com>
2022-01-12 00:29:17 +0900

    mib

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
-  belongs_to :post, optional: true
-  belongs_to :user
+  belongs_to :post, required: true
+  belongs_to :user, required: true
 
   validates_uniqueness_of :post_id, scope: :user_id    # バリデーション（ユーザーと記事の組み合わせは一意）
   # 同じ投稿を複数回お気に入り登録させないため。
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :post, optional: true
-  belongs_to :user
+  belongs_to :post, required: true
+  belongs_to :user, required: true
 
   validates_uniqueness_of :post_id, scope: :user_id
 end
