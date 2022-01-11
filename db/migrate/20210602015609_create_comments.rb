20a7565b910f860f3aa23bd43ba922c226c364ea
Niyomong <tt910208@gmail.com>
2022-01-12 00:01:50 +0900

    mib

--- db/migrate/20210602015609_create_comments.rb
+++ db/migrate/20210602015609_create_comments.rb
@@ -2,7 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :post
+      # , null: false, foreign_key: true
       t.text :commentContent
 
       t.timestamps
