f9e190a29f8b3b11b0083a943100e1116960f074
Niyomong <tt910208@gmail.com>
2022-01-12 01:11:24 +0900

    mib

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins ENV['FRONTEND_LOCAL_DOMAIN'] || ENV['FRONTEND_PRODUCTION_DOMAIN']
+        origins ENV['FRONTEND_PRODUCTION_DOMAIN']
 
         resource '*',
             headers: :any,
