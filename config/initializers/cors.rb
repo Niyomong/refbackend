087023cf67557407c47e7731347f4c12f8332766
Niyomong <tt910208@gmail.com>
2022-01-13 02:38:14 +0900

    mib

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'https://refnote-react-frontend.herokuapp.com'
+        origins ENV['FRONTEND_PRODUCTION_DOMAIN'], ENV['FRONTEND_LOCAL_DOMAIN']
 
         resource '*',
             headers: :any,
