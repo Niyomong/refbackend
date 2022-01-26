37c266ffb7c5ea71a5f5a3559f5ba8e427fc9afd
Niyomong <tt910208@gmail.com>
2022-01-27 00:47:18 +0900

    aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,11 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = ENV['S3_BUCKET']
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
@@ -10,9 +13,15 @@ CarrierWave.configure do |config|
       region: ENV['AWS_REGION'],
       path_style: true
     }
+    config.fog_directory  = ENV['S3_BUCKET']
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+
   else
     config.asset_host = ENV['BACKEND_LOCAL_DOMAIN'] #バックエンド側のドメイン名
     config.storage = :file
     config.cache_storage = :file
   end
 end
+
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
